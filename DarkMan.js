369e00f73857bb503a219e385f1b5ef9e0ca9464
BATMAN692 <82140753+BATMAN692@users.noreply.github.com>
2021-04-20 17:13:16 +0300

    Update DarkMan.js

--- DarkMan.js
+++ DarkMan.js
@@ -20,7 +20,7 @@ const dateFormat = require("dateformat");
 var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
-const prefix = "p!help";
+const prefix = "b!help";
 client.login("Nzk4NjEzMjU2MTcxMTU5NjQy.X_3kjQ.GUNLjAaLfeKXO8mcGhRgrRRCsJk");
 client.on("ready", async () => {
   console.log(`Logged in as ${client.user.username}!`);
